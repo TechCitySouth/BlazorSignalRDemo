242a31b76a42ce06ad5df8b85302e8ac857182c3
TechCitySouth <mike@techcitysouth.com>
2020-03-12 12:14:01 -0400

    Add project files.

---
+++ BlazorSignalR/Server/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MessagePack;
+using Microsoft.AspNetCore.SignalR;
+using BlazorSignalR.Shared;
+
+namespace BlazorSignalRApp.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(byte[] msgpack)
+        {
+            MPClass.MyMessage mc2 = MessagePackSerializer.Deserialize<MPClass.MyMessage>(msgpack);
+            await Clients.All.SendAsync("ReceiveMessage", msgpack);
+        }
+    }
+}
---
+++ BlazorSignalR/Shared/MPClass.cs
@@ -0,0 +1,19 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorSignalR.Shared
+{
+    public class MPClass
+    {
+        [MessagePackObject]
+        public class MyMessage
+        {
+            [Key(0)]
+            public string user { get; set; }
+            [Key(1)]
+            public string message { get; set; }
+        }
+    }
+}
